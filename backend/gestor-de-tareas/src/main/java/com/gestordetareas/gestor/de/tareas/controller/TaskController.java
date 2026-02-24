710bbb4900ce9d0207933655629398d07c2e89c0
Fran3103 <fn.aguirre@hotmail.com>
2026-02-24 09:25:10 -0300

    feat: agrego cors y configuraciones

--- backend/gestor-de-tareas/src/main/java/com/gestordetareas/gestor/de/tareas/controller/TaskController.java
+++ backend/gestor-de-tareas/src/main/java/com/gestordetareas/gestor/de/tareas/controller/TaskController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/tasks")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:5173")
 public class TaskController {
 
     final private TaskService taskService;
